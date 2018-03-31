b014244902fccd064ca63cb3de34a11c87e872b5
Tom1363 <tomsuzuki2633@gmail.com>
2018-03-31 17:16:30 +0900

    とりあえずframeでどうにか...
    でもこれ使いたくないんだよなあ...
    JavaScriptも嫌だし...

--- html/js/include.js
+++
@@ -1,4 +0,0 @@
-﻿function include(){
- var navdata = '<nav class="navbar"><h1 class="navh1">人工知能同好会</h1><ul class="navul nav"><li><a href="../index.html#top">TOP</a></li><li><a href="../index.html#about">ABOUT</a></li><li><a href="../index.html#log">LOG</a></li><li><a href="../index.html#update">UPDATE</a></li></ul></nav>'
- document.getElementById("target_nav").innerHTML = navdata;
-}
